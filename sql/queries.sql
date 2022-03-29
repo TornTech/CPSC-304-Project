2b9586d247be4a8e6da3e003eeb98cd2e311f70a
Ben Torn <tornben@gmail.com>
2022-03-29 15:02:05 -0700

    Removed old query

--- sql/queries.sql
+++ sql/queries.sql
@@ -12,11 +12,6 @@ SELECT T.modulenum, T.tlength, T.tname
 FROM Agent as A, Training as T, TrainingCompletion as TC
 WHERE A.AgentID = TC.AgentID AND T.ModuleNum = TC.ModuleNum AND A.agentid = 2;
 
-/* OLD - IGNORE FOR NOW*/
-SELECT A.AName, A.Email
-FROM Agent AS A, Training AS T, TrainingCompletion AS TC
-WHERE A.AgentID = TC.AgentID AND T.ModuleNum = TC.ModuleNum AND T.TName = 'Handling Difficult Customers';
-
 /* AGGREGATION: In English: List the name and salary of the agent who earns the highest agent salary in the company */
 SELECT AName, Salary AS HighestSalary
 FROM Agent
